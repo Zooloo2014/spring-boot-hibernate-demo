914ed5293c246061019aec028c19211206d75ea2
Brent Engelbrecht <brent@zooloo.co.za>
2018-08-21 16:21:08 +0200

    Initial commit

---
+++ src/main/java/com/example/springboothibernatedemo/SpringBootHibernateDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboothibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateDemoApplication.class, args);
+
+        System.out.printf("\nStarting SpringBootHibernateDemo\n\n");
+	}
+}
